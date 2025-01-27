c8c393ed46a15454c6d8206578ee6a3a850c1253
sudoLamen <nandor3292@gmail.com>
2025-01-27 18:55:40 +0100

    atirtam kb az egesz cuccost, valamint kilottem egy fuggvenyt ami ott volt de sehol sincs meghivva a kodban

--- dusza.py
+++ dusza.py
@@ -2,61 +2,70 @@ import os
 import string
 
 def monitoring(root_directory):
-    neve = []
-    azonosito = []
-    darab = []
     os.system("cls")
-    mappak = os.listdir(root_directory)
-    mappak.remove(".klaszter")
+    directories = [dir for dir in os.listdir(root_directory) if dir != ".klaszter"]
+    
+    for dir in directories:
+        print(dir)
+        config_path = os.path.join(root_directory, dir, ".szamitogep_config")
 
-    for i in range(len(mappak)):
-        print(mappak[i])
-        config = open(root_directory+f"\\{mappak[i]}\\.szamitogep_config")
-        config = config.readlines()
-        for j in range(len(config)):
-            lista = os.listdir(root_directory+f"\\{mappak[i]}")
-            lista.remove(".szamitogep_config")
+        if not os.path.isfile(config_path):
+            print(f"Hiba! Nincs konfigurációs fájl itt: {config_path}")
+            exit()
+
+        with open(config_path) as config_file:
+            config = [int(line.strip()) for line in config_file]
+
+        files = [file for file in os.listdir(os.path.join(root_directory, dir)) if file != ".szamitogep_config"]
+
         print(f"\tMAX \tElérhető")
-        for j in range(int(len(config)/2)):
-            proci_hasznalat = 0
-            ram_hasznalat = 0
-            for k in range(len(lista)):
-                hasznalt = open(root_directory+f"\\{mappak[i]}\\{lista[k]}")
-                hasznalt = hasznalt.readlines()
-                proci_hasznalat += int(hasznalt[2].strip())
-                ram_hasznalat += int(hasznalt[3].strip())
-            print("\t" + config[j].strip() + "\t" + str(int(config[j]) - proci_hasznalat))
-            print("\t" + config[j].strip() + "\t" + str(int(config[j]) - ram_hasznalat))
+
+        for j in range(0, len(config), 2):
+            max_cpu, max_ram = config[j], config[j + 1]
+            cpu_usage = 0
+            ram_usage = 0
+
+            for file in files:
+                file_path = os.path.join(root_directory, dir, file)
+                with open(file_path) as file:
+                    data = file.readlines()
+                    cpu_usage += int(data[2].strip())
+                    ram_usage += int(data[3].strip())
+            print(f"\t{max_cpu}\t{cpu_usage}")
+            print(f"\t{max_ram}\t{ram_usage}")
         print()
 
     return
 
 def add_instance(root_directory):
-    name = ''.join([x for x in input("Add meg a számítógép nevét: ")])
+    name = input("Add meg a számítógép nevét: ")
     resources = [int(x) for x in input("Add meg a millimagok számát és memóriakapacitást (MB) szóközzel elválasztva: ").split()]
-    letters = list(string.ascii_letters)
-    digits = list(string.digits)
+    valid_characters = list(string.ascii_letters) + list(string.digits)
+
+    instance_path = os.path.join(root_directory, name)
+
+    print(instance_path)
+
+    if os.path.isdir(instance_path):
+        print(f"Ilyen nevű számítógép már létezik! ({name})")
+        input()
+        return
 
     for c in name:
-        if c not in letters and c not in digits:
-            exit("baj van fonok")
+        if c not in valid_characters:
+            print(f"Hiba! Helytelen karakter: {c}")
+            input()
+            return
 
     os.makedirs(f"{root_directory}/{name}")
     with open(f"{root_directory}/{name}/.szamitogep_config", 'x') as file:
         file.write(f"{resources[0]}\n{resources[1]}")
 
-def bekeres(egyezni, kiiratas):
-    bemenet = int(input(kiiratas))
-    while bemenet != egyezni:
-        print("Rossz a bemenet")
-        bemenet = int(input(kiiratas))
-    return bemenet
-
 def display_menu():
     print("1: Monitoring\n2: Add Instance")
 
 def main():
-    befele = ""
+    user_input = ""
     root_directory = ""
     while True:
         cluster_name = input("Add meg a cluster mappa nevét: ")
@@ -66,14 +75,14 @@ def main():
         else:
             print(f"Hiba! Nem található {cluster_name} nevű mappa a jelenlegi könyvtárban. ({os.getcwd()})")
 
-    while befele.lower() != "stop":
+    while user_input.lower() != "stop":
         os.system("cls")
         display_menu()
-        befele = input().lower()
-        if befele == "1":
+        user_input = input()
+        if user_input == "1":
             monitoring(root_directory)
             input()
-        elif befele == "2":
+        elif user_input == "2":
             add_instance(root_directory)
     os.system("cls")
 
