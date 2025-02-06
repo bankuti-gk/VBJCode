1fceabd341540942be404d6fcbb7bd37f38a247a
sudoLamen <nandor3292@gmail.com>
2025-02-06 14:43:00 +0100

    alap gui cluster gombbal

--- main.py
+++ main.py
@@ -1,70 +1,78 @@
 import os
+from tkinter import *
+from tkinter import filedialog
 
 from monitoring import monitoring
 from utilities import display_menu, run_checks
 from management import add_instance, delete_instance
 
-possible_inputs = {
-    1 : monitoring,
-    2 : add_instance,
-    3 : delete_instance
-}
+def get_cluster_path():
+    cluster_path = filedialog.askdirectory(initialdir="/", title="Válassza ki a cluster mappát..")
+
+root = Tk()
+root.title("Cluster Command Center")
+root.geometry("800x500")
+get_cluster_button = Button(root, text="Cluster Megnyitása", command=get_cluster_path, activebackground="gray").pack(side=TOP)
 
 
-def main():
-    os.system("title Cluster Command Center")
-    while True:
-        os.system("cls")
-        print("Add meg a cluster pontos útvonalát!")
-        cluster_path = user_input = input(">> ").strip()
-        if os.path.exists(cluster_path):
-            break
-        else:
-            print(f"Hiba! Nem található mappa a megadott útvonalon: {cluster_path}")
-            input()
+root.mainloop()
 
-    while True:
-        os.system("cls")
-        run_checks(cluster_path)
-        display_menu()
-
-        user_input = input(">> ").strip()
-        
-        # <-------------------------------------------------->
-        # ez a rész érvényesíti az inputot
-
-        if not user_input:
-            print("HIBA: Nem lehet üres az input.")
-            input("Nyomj meg egy gombot a folytatáshoz..")
-            continue
-
-        if not user_input.isdigit():
-            print("\nHIBA: Kérlek, egy számot adj meg.")
-            input("Nyomj meg egy gombot a folytatáshoz..")
-            continue
-
-        # <-------------------------------------------------->
-
-        user_input = int(user_input)    # átalakítja az inputot integerré, hogy tudjon vele dolgozni
-
-        if user_input == 0:
-            break
-
-        if user_input in possible_inputs:
-            possible_inputs[user_input](cluster_path)
-            os.system("cls")
-        
-        else:
-                print("\nHIBA: Érvénytelen választás! (ha ezt a hibat kapod valami nagyon nem jo)")
-                input("Nyomj egy gombot a folytatáshoz...")
-                continue
-        
-
-if __name__ == "__main__":
-    main()
-
-<<<<<<< HEAD
-#asd
-=======
-#szia
->>>>>>> d9aee4c5a3adce4cb94b4984d392839c1f571914
+#-----PROGRAM-----
+#possible_inputs = {
+#    1 : monitoring,
+#    2 : add_instance,
+#    3 : delete_instance
+#}
+#
+#
+#def main():
+#    os.system("title Cluster Command Center")
+#    while True:
+#        os.system("cls")
+#        print("Add meg a cluster pontos útvonalát!")
+#        cluster_path = user_input = input(">> ").strip()
+#        if os.path.exists(cluster_path):
+#            break
+#        else:
+#            print(f"Hiba! Nem található mappa a megadott útvonalon: {cluster_path}")
+#            input()
+#
+#    while True:
+#        os.system("cls")
+#        run_checks(cluster_path)
+#        display_menu()
+#
+#        user_input = input(">> ").strip()
+#        
+#        # <-------------------------------------------------->
+#        # ez a rész érvényesíti az inputot
+#
+#        if not user_input:
+#            print("HIBA: Nem lehet üres az input.")
+#            input("Nyomj meg egy gombot a folytatáshoz..")
+#            continue
+#
+#        if not user_input.isdigit():
+#            print("\nHIBA: Kérlek, egy számot adj meg.")
+#            input("Nyomj meg egy gombot a folytatáshoz..")
+#            continue
+#
+#        # <-------------------------------------------------->
+#
+#        user_input = int(user_input)    # átalakítja az inputot integerré, hogy tudjon vele dolgozni
+#
+#        if user_input == 0:
+#            break
+#
+#        if user_input in possible_inputs:
+#            possible_inputs[user_input](cluster_path)
+#            os.system("cls")
+#        
+#        else:
+#                print("\nHIBA: Érvénytelen választás! (ha ezt a hibat kapod valami nagyon nem jo)")
+#                input("Nyomj egy gombot a folytatáshoz...")
+#                continue
+#        
+#
+#if __name__ == "__main__":
+#    main()
